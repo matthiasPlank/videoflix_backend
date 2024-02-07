b841ab04c8af2aec8660055961a135f3aa10821d
Matthias Plank <matthias.plank@gmx.at>
2024-02-07 20:04:07 +0100

    add restframework

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'videos', 
 ]
 
@@ -130,3 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -18,7 +18,20 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.urls import include, path
+from rest_framework import routers
+
+from videos import views
+
+
+
+router = routers.DefaultRouter()
+router.register(r'video', views.VideoViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += router.urls
