6e2570941fa04889c49b86467a63055e36ebbe31
Matthias Plank <matthias.plank@gmx.at>
2024-02-09 22:29:30 +0100

    modify model

--- videoflix/videos/serializers.py
+++ videoflix/videos/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from .models import Video
 
 class VideoSerializer(serializers.HyperlinkedModelSerializer):
+    
+    id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Video
         fields = '__all__'
