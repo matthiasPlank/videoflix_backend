8eea5dd8432ce8851d868139c56221b2eaee9ace
SebSeitz <sebastian_seitz13@web.de>
2024-02-17 21:42:51 +0100

    added postVideo method

--- videoflix/videos/views.py
+++ videoflix/videos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import permissions, viewsets
-
+from django.http import HttpResponse
 from .serializers import VideoSerializer
 from .models import Video
 
@@ -12,3 +12,15 @@ class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
     #permission_classes = [permissions.IsAuthenticated]
+
+    def postVideo(self, request, *args, **kwargs):
+        title = request.data['title']
+        description = request.data['description']
+        video_file = request.data['video_file']
+        poster_file = request.data['poster_file']
+        genre = request.data['genre']
+
+        Video.objects.create(title=title, description = description,
+                             video_file = video_file, poster_file = poster_file,
+                             genre = genre)
+        return HttpResponse({'message': 'Video has been added'}, status=200)
