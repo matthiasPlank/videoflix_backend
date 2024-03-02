058fb1a5cbe3a13a274218e7185ea6cb0de343f8
SebSeitz <sebastian_seitz13@web.de>
2024-03-02 22:03:19 +0100

    1st test for post_save video conversion

--- videoflix/videos/tests.py
+++ videoflix/videos/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from unittest.mock import patch
+from videos.models import Video
+from django.db.models.signals import post_save
 
-# Create your tests here.
+class VideoSignalTests(TestCase):
+
+    def setUp(self):
+        # Provide the content of a real video file
+        with open('media/videos/testvideos/ants.mp4', 'rb') as file:
+            video_content = file.read()
+
+        # Create a Video instance with the real video content
+        video_file = SimpleUploadedFile("video.mp4", video_content, content_type="video/mp4")
+        self.video = Video.objects.create(video_file=video_file)
+
+    @patch('videos.signals.convert_480p')
+    @patch('videos.signals.convert_720p')
+    def test_video_post_save_method(self, mock_convert_720p, mock_convert_480p):
+        # Call the post_save signal manually
+        post_save.send(sender=Video, instance=self.video, created=True)
+
+        # Assert that the convert_480p and convert_720p functions were called with the correct arguments
+        mock_convert_480p.assert_called_once_with(self.video.video_file.path)
+        mock_convert_720p.assert_called_once_with(self.video.video_file.path)
+
+        # Refresh the instance from the database to get the latest changes
+        self.video.refresh_from_db()
+        self.assertTrue(self.video.video_file.path)
+
