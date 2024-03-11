bc1f44254a3d2f5d9a4898f78a9256a1ae7500fa
Matthias Plank <matthias.plank@gmx.at>
2024-03-11 20:19:17 +0100

    add Static url

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -43,6 +43,6 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path('django-rq/', include('django_rq.urls')),
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += router.urls
